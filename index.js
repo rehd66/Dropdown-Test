b7e7972f68167f242c5cf3a5afd302c16b472d5c
Jason Redmon <rehd66@gmail.com>
2021-09-10 22:01:21 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const show = document.getElementById("show");
+const hide = document.getElementById("hide");
+
+function showSub() {
+    hide.style.display = 'block';
+}
+function hideSub() {
+    hide.style.display = 'none';
+}
